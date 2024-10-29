0988a43c1b3c20e25ed091b7a3b794e502f01d87
Dilovan Candan <dcandan@k2r2p9.42mulhouse.fr>
2024-10-29 16:39:21 +0100

    finis

--- P00/ex01/format_ft_time.py
+++ P00/ex01/format_ft_time.py
@@ -1,13 +1,12 @@
 import time
 
 current = time.time()
-old_date = time.strftime("%b %d %Y", time.gmtime(0))
 
 formated_curr = f"{current:,.3f}"
 scientific_curr = f"{current:,.3e}"
 date = time.strftime("%b %d %Y", time.gmtime())
 
 
-print("Seconds since", old_date, ":", formated_curr, "or",
+print("Seconds since January 1, 1970:", formated_curr, "or",
       scientific_curr, "in scientific notation")
 print(date)
--- P00/ex08/Loading.py
+++ P00/ex08/Loading.py
@@ -15,24 +15,19 @@ def ft_tqdm(lst: range) -> None:
       the range in real time.
     - Pauses between iterations with a custom delay using `ft_sleep`.
     """
-    for elem in lst:
-        milestone = 0
-        percent = (elem / lst.stop) * 100
-        if percent == 0 or percent > milestone + 1:
-            milestone = percent
-            load_bar = int((elem / lst.stop) * 100)
-        loading = "|[" + "=" * load_bar + ">" + " " * (100 - load_bar) + "]|"
-        print(f" {int(percent)}%", loading, elem, "/", lst.stop, end='\r')
-        ft_sleep(1000000)
-        yield elem
-    elem += 1
-    loading = loading[:-3] + "=>]"
-    print("100%", loading, elem, "/", lst.stop, end='\r')
-    ft_sleep(50000000)
-    return
-
-
-def ft_sleep(sleep):
-    """Custom sleep without time library"""
-    for _ in range(sleep):
-        pass
+    try:
+        for elem in lst:
+            milestone = 0
+            percent = (elem / lst.stop) * 100
+            if percent == 0 or percent > milestone + 1:
+                milestone = percent
+                load_bar = int((elem / lst.stop) * 100)
+            load = "|[" + "=" * load_bar + ">" + " " * (100 - load_bar) + "]|"
+            yield (print(f" {int(percent)}%",
+                         load, elem, "/", lst.stop, end='\r'))
+        elem += 1
+        load = load[:-4] + "==>]"
+        print("100%", load, elem, "/", lst.stop, end='\r')
+        return
+    except (Exception, KeyboardInterrupt) as msg:
+        print(msg)
