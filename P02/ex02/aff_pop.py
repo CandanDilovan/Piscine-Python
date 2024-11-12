8d27a92c450dd5d8fef5f7cdd6bf5058f3a072e4
Dilovan Candan <dcandan@k2r3p8.42mulhouse.fr>
2024-11-12 16:04:07 +0100

    ajouter des __doc__ au P01

--- P02/ex02/aff_pop.py
+++ P02/ex02/aff_pop.py
@@ -1,21 +1,31 @@
 import matplotlib.pyplot as plt
+from matplotlib.ticker import EngFormatter
 from load_csv import load
 import pandas as pd
 
 
 def aff_live(path: str):
     loaded = load(path)
-    loaded = loaded.__str__.replace("M", "000000")
-    loaded = loaded.str.replace(".", "")
     loaded.set_index('country', inplace=True)
-    print(loaded.loc['France'])
-    french_plot = loaded.loc['France']
-    china_plot = loaded.loc['China']
-    french_plot.plot(title="Population Projections", xlabel="Year", ylabel="Population")
-    china_plot.plot()
+    french_plot = loaded.loc['France'].apply(multiply)
+    pakistan_plot = loaded.loc['Pakistan'].apply(multiply)
+    plot = french_plot.plot(title="Population Projections", xlabel="Year",
+                            ylabel="Population", label='France')
+    pakistan_plot.plot(label='PAKISTAN100%CRAZY')
+    plot.yaxis.set_major_formatter(EngFormatter())
     plt.legend()
     plt.show()
-    
+
+
+def multiply(row):
+    if 'k' in row:
+        print("stoopid")
+        row = round(float(row[:-1]) * 1000)
+    elif 'M' in row:
+        row = round(float(row[:-1]) * 1000000)
+    elif 'B' in row:
+        row = round(float(row[:-1]) * 1000000000)
+    return row
 
 
 aff_live("population_total.csv")
