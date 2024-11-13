10a9ab67622fc6639812d6b34172fca359c9798a
Dilovan Candan <dcandan@k2r3p4.42mulhouse.fr>
2024-11-13 12:24:32 +0100

    ajouter des __doc__ au P01 et P02

--- P02/ex01/aff_life.py
+++ P02/ex01/aff_life.py
@@ -1,16 +1,15 @@
 import matplotlib.pyplot as plt
 from load_csv import load
-import pandas as pd
 
 
-def aff_live(path: str):
+def main(path: str):
     loaded = load(path)
     loaded.set_index('country', inplace=True)
     print(loaded.loc['France'])
     plot = loaded.loc['France']
     plot.plot(title="France life expectancy Projections", xlabel="Years", ylabel="Life expentanct")
     plt.show()
-    
 
 
-aff_live("life_expectancy_years.csv")
+if __name__ == "__main__":
+    main("life_expectancy_years.csv")
--- P02/ex02/aff_pop.py
+++ P02/ex02/aff_pop.py
@@ -1,20 +1,23 @@
 import matplotlib.pyplot as plt
 from matplotlib.ticker import EngFormatter
 from load_csv import load
-import pandas as pd
 
 
-def aff_live(path: str):
-    loaded = load(path)
-    loaded.set_index('country', inplace=True)
-    french_plot = loaded.loc['France'].apply(multiply)
-    pakistan_plot = loaded.loc['Pakistan'].apply(multiply)
-    plot = french_plot.plot(title="Population Projections", xlabel="Year",
-                            ylabel="Population", label='France')
-    pakistan_plot.plot(label='PAKISTAN100%CRAZY')
-    plot.yaxis.set_major_formatter(EngFormatter())
-    plt.legend()
-    plt.show()
+def main(path: str):
+    try:
+        loaded = load(path)
+        loaded.set_index('country', inplace=True)
+        french_plot = loaded.loc['France'].apply(multiply)
+        pakistan_plot = loaded.loc['Pakistan'].apply(multiply)
+        pakistan_plot, french_plot = pakistan_plot[:-50], french_plot[:-50]
+        plot = french_plot.plot(title="Population Projections", xlabel="Year",
+                                ylabel="Population", label='France')
+        pakistan_plot.plot(label='PAKISTAN100%CRAZY')
+        plot.yaxis.set_major_formatter(EngFormatter())
+        plt.legend()
+        plt.show()
+    except (FileNotFoundError, IsADirectoryError, KeyboardInterrupt) as msg:
+        print(msg)
 
 
 def multiply(row):
@@ -28,4 +31,5 @@ def multiply(row):
     return row
 
 
-aff_live("population_total.csv")
+if __name__ == "__main__":
+    main("population_total.csv")
